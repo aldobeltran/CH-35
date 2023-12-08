ca2b08118f144e26688ba51033850626b55cfcda
aldobeltran <javsssantos@gmail.com>
2023-12-07 21:14:23 -0600

    Repo principal

---
+++ javascript/javascript1/js/script.js
@@ -0,0 +1,34 @@
+// comentarios en una sola linea
+
+/*comentario 
+multilinea*/
+
+/*alert("Hola mundoo");*/
+
+//console.log("Este es un mensaje")
+
+
+/*var nombre = prompt("ingresa tu nombre");
+alert("Hola" + nombre + "Bienvenido a la ch35");*/
+
+/*var numero1 = parseInt (prompt("Ingresa el primer numero:"));
+var numero2 = parseInt (prompt("Ingresa el segundo nuero:"));*/
+
+
+/*console.log("suma:" + (numero1 + numero2));
+console.log("resta:" + (numero1 - numero2));
+console.log("multiplicacion:" + (numero1 * numero2));
+console.log("division:" + (numero1 / numero2));*/
+
+
+var nombre = prompt("Ingresa tu nombre");
+
+var edad = parseInt(prompt("Ingresa tu edad"));
+
+if ( edad < 17 ){
+
+    alert(nombre + ". Tas chavo, chavo");
+}
+else{
+    alert(nombre + ". Ya te sabes la de chambear");
+}
