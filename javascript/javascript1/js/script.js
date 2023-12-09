761a555c6231f2a0448a467b624c2b5906aa6f03
aldobeltran <javsssantos@gmail.com>
2023-12-09 01:09:01 -0600

    figmaproyect beta

--- javascript/javascript1/js/script.js
+++ javascript/javascript1/js/script.js
@@ -12,15 +12,15 @@ multilinea*/
 alert("Hola" + nombre + "Bienvenido a la ch35");*/
 
 /*var numero1 = parseInt (prompt("Ingresa el primer numero:"));
-var numero2 = parseInt (prompt("Ingresa el segundo nuero:"));*/
-
-
-/*console.log("suma:" + (numero1 + numero2));
+var numero2 = parseInt (prompt("Ingresa el segundo nuero:"));
+*/
+/*
+console.log("suma:" + (numero1 + numero2));
 console.log("resta:" + (numero1 - numero2));
 console.log("multiplicacion:" + (numero1 * numero2));
-console.log("division:" + (numero1 / numero2));*/
-
-
+console.log("division:" + (numero1 / numero2));
+*/
+/*
 var nombre = prompt("Ingresa tu nombre");
 
 var edad = parseInt(prompt("Ingresa tu edad"));
@@ -32,3 +32,23 @@ if ( edad < 17 ){
 else{
     alert(nombre + ". Ya te sabes la de chambear");
 }
+*/
+/*
+Scope: es el alcance de nuestras variables
+Var, let, const la diferencia es el alcance
+var tiene un alcance global
+let tiene un alcance local
+const se muestra sin cambios
+
+String: Cadena de texto ("lleva comillas")
+number: numero
+noolena: true/false
+null: nulo
+undefinied: no esta asignado o definido
+*/
+let nombre = "Dani";
+let invitadosExtra = 2;
+let esMayorEdad = true;
+let edad = 24;
+let invitadoEspecial = null;
+let horaDeSalida = undefined;
